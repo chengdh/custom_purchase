2af64fb1eee32f702d1ab32d58a553836d7ab818
chengdh <cheng.donghui@gmail.com>
2013-06-23 16:08:34 +0800

    first commit

---
+++ __openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'custom purchase',
+    'version': '0.1',
+    'category': 'Purchase Management',
+    'description': """定制修改purchase的功能""",
+    'author': 'chengdh (cheng.donghui@gmail.com)',
+    'website': '',
+    'license': 'AGPL-3',
+    'depends': ['purchase', 'web'],
+    'init_xml': [],
+    'update_xml': [],
+    'demo_xml': [],
+    'active': False,
+    'installable': True,
+    'web':True,
+    'css': [],
+    'js': [],
+}
+
